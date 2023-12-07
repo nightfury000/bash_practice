b15117ad8c4eb732788c699b38786255fc054121
Chandrakant <csborle@gmail.com>
2023-12-07 23:06:29 +0530

    variable example 2

---
+++ variables/var_script2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Assign a string to a variable
+name = "John Doe"
+
+#Print the variable
+echo "Hello, $name!"
+
+# Assign the result of a command to a variable
+current_date=$(date +%Y-%m-%d)
+
+#Print the variable
+echo "Today's date is: $current_date"
+
