874cf3b25bafdba9d8f3bd2e8844eff07ad57f29
Chandrakant <csborle@gmail.com>
2023-12-16 22:19:53 +0530

    variable_example_new

---
+++ variables/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Setup and configure server"
+
+file_name=config.yaml
+config_files=$(ls config)
+
+echo "using file $file_name to configure something"
+echo "here are all configuration files: $config_files"
+
+
