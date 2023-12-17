9be39320b881408e35ea5988398ef2fc19acd081
Chandrakant <csborle@gmail.com>
2023-12-17 23:35:41 +0530

    added note

--- variables/setup.sh
+++ variables/setup.sh
@@ -3,7 +3,8 @@
 echo "Setup and configure server"
 
 file_name=config.yaml
-config_files=$(ls config)
+
+config_files=$(ls config)  	# in this line ls config is added in bracket to consider in as a single command that goes in variable config_files
 
 echo "using file $file_name to configure something"
 echo "here are all configuration files: $config_files"
