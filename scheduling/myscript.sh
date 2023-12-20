0d435eab7f773e35e71ca287c034900729471f29
Chandrakant <csborle@gmail.com>
2023-12-20 21:59:09 +0530

    at command

--- scheduling/myscript.sh
+++ scheduling/myscript.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+#There is at command (at  and  batch read commands from standard input or a specified file which are to be executed at a later time, using /bin/sh).
+
+
 logfile=job_results.log
 
 echo"The script ran at the following time: $(date)" > $logfile
