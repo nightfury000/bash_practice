0b329b5dfa3a74e4f2f87357641228fe743229a7
Chandrakant <csborle@gmail.com>
2023-12-25 22:28:30 +0530

    sed example

---
+++ handy-tools/replace_is_by_the.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#this script will replace 'is' by 'the'
+
+filename="sample.txt"
+
+#Take the search string (-p is for prompt text, it outputs the prompt string before reading user input)
+read -p "Enter the search string: " search
+
+#Take the replace string
+read -p "Enter the replace string " replace
+
+#the we make the search and replace as a variable and pass it to the sed command as the syntax below
+if [[ $search != "" && $replace != "" ]] ; then
+	sed -i "s/$search/$replace" $filename
+fi
+
+
