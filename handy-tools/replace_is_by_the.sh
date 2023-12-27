cff8a427e832c05b542a568fe5515d8f2304592b
Chandrakant <csborle@gmail.com>
2023-12-27 22:22:33 +0530

    need to do using variable

--- handy-tools/replace_is_by_the.sh
+++ handy-tools/replace_is_by_the.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+: '
+#!/bin/bash
 
+# Specify the filename to modify
+filename="your_text_file.txt"
+
+# Perform the replacement using sed
+sed -i 's/is/the/g' "$filename"
+
+# Print a message to confirm the replacement
+echo "Replacement completed in $filename."
+
+this above script works fine but if when we use variable it gives some error.
+
+
+'
 #this script doesn't gives error like "sed: -e expression #1, char 8: unterminated `s' command"
 
 #this script will replace 'is' by 'the'
